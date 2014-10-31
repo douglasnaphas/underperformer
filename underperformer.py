936aacc609274c397015e1b408a3e0be7c28fb15
Douglas Naphas <dnaphas@sevone.com>
2014-10-31 00:32:40 -0400

    Removed web files. underperformer.py gets weekly worst employee

--- underperformer.py
+++ underperformer.py
@@ -2,36 +2,44 @@
 
 # Find the underperformer.
 
-# constants
-	# default params
+import argparse
+import urllib
+import json
 
-# Get command line options: --file=FILE, --url=URL, --threshold=NUMBER, --help, --stdevs=STDEVS
+VERSION = '0.1.0'
+INPUT_LIMIT = 2**30 # The string of data must be 1 G or less.
+# Array key names.
+EMPLOYEE = 'employee'
+WORKCOUNT = 'workCount'
+TOTAL = 'total'
+WEEKS = 'w'
 
-# Read in source.
+class MyError( Exception ): # TODO: Is there really no built-in way to do this?
+	def __init__( self, value ):
+		self.value = value
+	def __str__( self ):
+		return repr( self.value )
 
-# weeks = json_decode( source )
+# Return the employee id of the worst employee this week, considering all past weeks.
+# @param array emps Array of employees, empid => { TOTAL : total production, WEEKS : weeks }
+def weeks_worst( emps ):
+	worst_id = emps.keys()[0]
+	worst_av = ( emps[worst_id][TOTAL] + 0.0 ) / emps[worst_id][WEEKS] # TODO: handle floats better
+	for emp in emps:
+		if ( ( emps[emp][TOTAL] + 0.0 ) / emps[emp][WEEKS] ) < worst_av:
+			worst_id = emp
+			worst_av = ( emps[worst_id][TOTAL] + 0.0 ) / emps[worst_id][WEEKS]
+	return worst_id
 
-# prod = array() # empl_num => running average production per week, number of weeks.
-		     # The workforce may vary.
-# worst_id = null
+def prod_stddev( prod ):
+	# get the array of averages, empid => av
+	# get the average of the array of averages
+	# for each in the array of averages, get the variance
+	# get the average variance
+	# get the stddev
+	return 0
 
-# underperformer = null
 
-# while ( week < count( weeks ) && !underperformer )
-	# for emp in weeks[week] # Get running average weekly production for each.
-		# empid = weeks[week][emp][employee]
-		# if ( array_key_exists( empid in prod ) )
-			# Update totals.
-			# prod[empid][total] += weeks[week][emp][workCount]
-			# prod[empid][w]++
-	# Find the worst employee (all weeks).
-	# underperformer = underperf( prod )
-
-# if !underperformer
-	# print no_underperf_message()
-# else
-	# print underperformer_message( prod )
-		
 
 # ( prod[] -> id of the underperformer, or null if there is none )
 # function underperf( array prod[empid=>[total, weeks]], function test( worst_id, prod[empid=>
@@ -41,3 +49,74 @@
 # function stdev_test
 
 # function avg_test
+
+# Get command line options.
+parser = argparse.ArgumentParser( description='Find the underperformer.' )
+parser.add_argument( '--version', help='print the current version and exit', action='version', version=VERSION )
+source_group = parser.add_mutually_exclusive_group( required = True )
+source_group.add_argument( '--file', '-f', help = 'read performance data from FILE', type = file )
+source_group.add_argument( '--url', '-u', help = 'read performance data from URL' )
+calc_group = parser.add_mutually_exclusive_group( required = True )
+calc_group.add_argument( '--stddevs', '-s', help = 'an underperformer is STDDEVS standard deviations from the next worse performer', type = float )
+calc_group.add_argument( '--ratio', '-r', help = 'an underperformer produces RATIO * the average of everyone else, or worse', type = float )
+args = parser.parse_args()
+
+# Read in source.
+if args.file:
+	# Try to read the file to a string.
+	try:
+		source_string = args.file.read( INPUT_LIMIT )
+		args.file.close
+	except:
+		print "Error reading from %s" % args.file.name
+else:
+	# Try to read the URL to a string.
+	try:
+		source_page = urllib.urlopen( args.url )
+		source_string = source_page.read( INPUT_LIMIT )
+		source_page.close
+	except:
+		print "Error collecting data from URL %s" % args.url
+	
+# weeks = json_decode( source_string )
+try:
+	weeks = json.loads( source_string )
+except:
+	print "Error decoding source JSON data."
+
+prod = {} # Empty dictionary. Will be: empl_num => [ running total production, num of weeks]. The workforce may vary.
+worst_id = None # this week
+underperformer = None # overall
+week = 0
+
+try:
+	while week < len(weeks) and not underperformer:
+		emp = 0
+		while emp < len ( weeks[week] ):
+			if EMPLOYEE in weeks[week][emp]:
+				empid = weeks[week][emp][EMPLOYEE]
+			else:
+				raise MyError("Bad data at week %d, employee %d (0-based)" % ( week, emp ) )
+			if not empid in prod:
+				prod[empid] = { TOTAL : 0, WEEKS : 0 }
+			prod[empid][TOTAL] += weeks[week][emp][WORKCOUNT]
+			prod[empid][WEEKS] += 1
+			print ( prod[empid][TOTAL] + 0.0 ) / prod[empid][WEEKS]
+			emp += 1
+		worst_id = weeks_worst( prod )
+		print "The worst employee this week has id %d" % worst_id
+		# underperformer = underperf( prod )
+		print ""
+		week += 1
+except MyError as ie:
+	print ie.value
+
+# if !underperformer
+	# print no_underperf_message()
+# else
+	# print underperformer_message( prod )
+		
+
+
+
+
